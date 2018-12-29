6b617706b0d82da604bdbc61c6ec88fbe6fa7d3b
yanshoutong <yanshoutong@sina.cn>
2018-12-29 12:01:40 +0800

    stage
    Signed-off-by: yanshoutong <yanshoutong@sina.cn>

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,24 @@
 #! /bin/sh
 
 echo "** deploy ** " $*
+
+REPO=.repo
+
+if [ $# != 3 ]; then
+    echo "arguments missing"
+    exit 1
+fi
+
+remote_branch=$3
+
+if [ ! -d $REPO ]; then
+    echo "$REPO folder does not exist, create it"
+    mkdir $REPO
+fi
+
+cd $REPO
+echo "start to clone $remote_branch"
+
+
+
+
